f76707d27b2ca4f94444ee333a6fe39b21820e2c
Drew Niman <drewmniman@gmail.com>
2023-12-07 13:27:49 -0800

    restrict welcome page info to JWT and user ID

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def logout():
 
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("home.html", session=session.get('user'))
 
 # ================================ USER DATASTORE FUNCTIONS ================================
 def get_user_id_by_sub(sub):
